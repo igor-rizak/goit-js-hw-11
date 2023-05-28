a00ea6e116e0ab51332734c75a16c4504822d314
igor-rizak <igor.rizak@ukr.net>
2023-05-28 23:26:00 +0300

    commit

---
+++ src/JS/images-service.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+// import Notiflix from 'notiflix';
+
+const API_KEY = '36675802-f1fae2e3ce3b586e4e267c1aa';
+const BASE_URL = 'https://pixabay.com/api';
+
+const searchParams = new URLSearchParams({
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: 40,
+});
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getImage() {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${searchParams}&page=${this.page}`
+    );
+    this.page += 1;
+
+    return response.data;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
